28c99b923d9c168ba70539e38299d319795adabe
mohdsesh <144240089+mohdsesh@users.noreply.github.com>
2023-10-03 09:13:21 +0330

    array-object

---
+++ java2.js
@@ -0,0 +1,15 @@
+const courses =[
+    {
+        id : 1,
+        name : 'html',
+        time : 10,
+        iscompleted : false
+    },
+    {
+        id : 2,
+        name : 'css',
+        time : 15,
+        iscompleted : false
+    }
+]
+console.log(courses);
